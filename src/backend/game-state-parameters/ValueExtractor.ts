872ecee606f9353a00614a3f18d3768a6db723b7
GravityTwoG <crytekov@gmail.com>
2024-04-14 17:52:17 +0700

    feat(gameStateValues): add array indexing support

--- src/backend/game-state-parameters/ValueExtractor.ts
+++ src/backend/game-state-parameters/ValueExtractor.ts
@@ -20,7 +20,7 @@ export class ValueExtractor {
         await this.converters["sav-to-json"].convert(
           filePath,
           pipelineItem.inputFilename,
-          pipelineItem.outputFilename
+          pipelineItem.outputFilename,
         );
         createdFiles.push(path.join(filePath, pipelineItem.outputFilename));
       }
@@ -32,12 +32,12 @@ export class ValueExtractor {
       path.join(filePath, gameStateParameters.filename),
       {
         encoding: "utf-8",
-      }
+      },
     );
 
     const gameStateValues = this.extractValues(
       JSON.parse(json),
-      gameStateParameters
+      gameStateParameters,
     );
 
     await Promise.allSettled(createdFiles.map((file) => fs.unlink(file)));
@@ -47,7 +47,7 @@ export class ValueExtractor {
 
   private extractValues(
     input: JSONType,
-    schema: GameStateParameters
+    schema: GameStateParameters,
   ): {
     gameStateParameterId: string;
     value: string;
@@ -60,6 +60,15 @@ export class ValueExtractor {
     for (const field of schema.parameters) {
       const value = this.readByKey(field.key, input);
 
+      if (Array.isArray(value)) {
+        for (const subValue of value) {
+          gameStateValues.push({
+            gameStateParameterId: field.id,
+            value: subValue,
+          });
+        }
+      }
+
       if (!isObject(value) && !Array.isArray(value) && value !== null) {
         gameStateValues.push({
           gameStateParameterId: field.id,
@@ -71,24 +80,47 @@ export class ValueExtractor {
     return gameStateValues;
   }
 
-  private readByKey(key: string, input: JSONType): string | null {
+  private readByKey(key: string, input: JSONType): string | string[] | null {
     const keys = key.split(".");
     let currentValue = input;
 
     for (let i = 0; i < keys.length; i++) {
       const key = keys[i];
-      const value = currentValue[key];
+      const isLastKey = i === keys.length - 1;
 
-      if (typeof value === "number") {
+      let value = currentValue[key];
+      if (this.isArrayIndex(key) && Array.isArray(currentValue)) {
+        if (key === "[*]") {
+          const values: string[] = [];
+          const restKey = keys.slice(i + 1).join(".");
+          for (let j = 0; j < currentValue.length; j++) {
+            const elem = isLastKey
+              ? currentValue[j]
+              : this.readByKey(restKey, currentValue[j]);
+
+            if (this.isPrimitiveType(elem) && isLastKey) {
+              values.push(elem.toString());
+            }
+          }
+          return values;
+        } else {
+          const index = parseInt(key.slice(1, -1));
+          value = currentValue[index];
+        }
+      } else if (this.isArrayIndex(key) && !Array.isArray(currentValue)) {
+        return null;
+      }
+
+      if (this.isPrimitiveType(value) && isLastKey) {
         return value.toString();
       }
-      if (typeof value === "boolean") {
-        return value.toString();
-      }
-      if (typeof value === "string") {
-        return value;
-      }
-      if (value === null || !isObject(value) || Array.isArray(value)) {
+
+      if (
+        value === null ||
+        value === undefined ||
+        !isObject(value) ||
+        !Array.isArray(value)
+      ) {
         return null;
       }
 
@@ -96,6 +128,18 @@ export class ValueExtractor {
     }
 
     return null;
+  }
+
+  private isArrayIndex(key: string): boolean {
+    return /^\[(\d+)|(\*)\]$/.test(key);
+  }
+
+  private isPrimitiveType(value: unknown): value is number | boolean | string {
+    return (
+      typeof value === "number" ||
+      typeof value === "boolean" ||
+      typeof value === "string"
+    );
   }
 }
 

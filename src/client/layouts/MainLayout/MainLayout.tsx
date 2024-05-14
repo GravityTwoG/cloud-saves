9ed41145267bce418b7024b74be3213264e1fd10
GravityTwoG <crytekov@gmail.com>
2024-05-14 20:32:33 +0700

    feat(ui): add go back button, change paper shadow in dark mode

--- src/client/layouts/MainLayout/MainLayout.tsx
+++ src/client/layouts/MainLayout/MainLayout.tsx
@@ -2,8 +2,11 @@ import classes from "./main-layout.module.scss";
 
 import { navLinks } from "@/client/config/navLinks";
 
+import { Container } from "@/client/ui/atoms/Container";
 import { Sidebar } from "@/client/lib/components/Sidebar/Sidebar";
 import { Footer } from "@/client/lib/components/Footer";
+import { GoBack } from "@/client/lib/components/GoBack";
+import { clsx } from "clsx";
 
 export type MainLayoutProps = {
   children: React.ReactNode;
@@ -15,7 +18,13 @@ export const MainLayout = ({ children }: MainLayoutProps) => {
       <div className={classes.DragBar} />
       <Sidebar links={navLinks} />
 
-      <main className="custom-scrollbar">
+      <main className={clsx("custom-scrollbar", classes.Main)}>
+        <div className={classes.Header}>
+          <Container>
+            <GoBack />
+          </Container>
+        </div>
+
         <div className={classes.Content}>{children}</div>
         <Footer />
       </main>

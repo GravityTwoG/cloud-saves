f62b4201d6a863f7fc753c7c554b2808ea8fa290
GravityTwoG <crytekov@gmail.com>
2024-06-14 16:50:25 +0700

    feat(ui): add View Transitions API tag, add game state example

--- src/client/entities/GameStateCard/GameStateCard.tsx
+++ src/client/entities/GameStateCard/GameStateCard.tsx
@@ -5,6 +5,7 @@ import { GameState } from "@/types";
 import { useConfirmModal } from "@/client/ui/hooks/useConfirmModal/useConfirmModal";
 
 import classes from "./game-state-card.module.scss";
+import { ReactTagProps } from "@/client/ui/types";
 
 import SyncIcon from "@/client/ui/icons/Sync.svg";
 import { FadedCard } from "@/client/ui/atoms/FadedCard";
@@ -13,6 +14,7 @@ import { ThreeDotsMenu } from "@/client/ui/molecules/ThreeDotsMenu";
 export type GameStateCardProps = {
   gameState: GameState;
   className?: string;
+  style?: ReactTagProps<"div">["style"];
   onDelete?: (gameStateId: string) => void;
   showSyncSettings?: boolean;
   href: string;
@@ -32,6 +34,7 @@ export const GameStateCard = (props: GameStateCardProps) => {
       imageURL={props.gameState.gameImageURL}
       href={props.href}
       className={clsx(classes.GameStateCard, props.className)}
+      style={props.style}
     >
       {props.onDelete && (
         <ThreeDotsMenu

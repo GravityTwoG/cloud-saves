3ed6d330bb3c5c3de7347f45cd8ea97c4e76f0d7
GravityTwoG <crytekov@gmail.com>
2024-06-19 22:03:32 +0700

    feat(ui): extract app buttons, change styles

--- src/client/app/components/Sidebar/Sidebar.tsx
+++ src/client/app/components/Sidebar/Sidebar.tsx
@@ -29,46 +29,7 @@ export type SidebarProps = {
 };
 
 export const Sidebar = (props: SidebarProps) => {
-  const { logout } = useAuthContext();
-  const { osAPI } = useAPIContext();
-  const { notify } = useUIContext();
-  const { theme, toggleTheme } = useThemeContext();
-  const { t, i18n } = useTranslation();
-  const [language, setLanguage] = usePersistedState<string>(
-    "language",
-    i18n.language,
-  );
-
-  useEffect(() => {
-    i18n.changeLanguage(language);
-  }, [language]);
-
-  useEffect(() => {
-    // run after changes in DOM
-    Promise.resolve().then(() => {
-      const bodyStyle = getComputedStyle(document.body);
-      const backgroundColor = bodyStyle.getPropertyValue("--color-background");
-      const textColor = bodyStyle.getPropertyValue("--color-text");
-      osAPI.setTitleBarSettings({
-        backgroundColor: `${backgroundColor}20`,
-        symbolColor: textColor,
-      });
-    });
-  }, [theme, osAPI]);
-
-  const toggleLanguage = () => {
-    const newLanguage = language === "en" ? "ru" : "en";
-    i18n.changeLanguage(newLanguage);
-    setLanguage(newLanguage);
-  };
-
-  const onLogout = async () => {
-    try {
-      await logout();
-    } catch (error) {
-      notify.error(error);
-    }
-  };
+  const { t } = useTranslation();
 
   return (
     <aside
@@ -107,39 +68,103 @@ export const Sidebar = (props: SidebarProps) => {
           [classes.Expanded]: props.isExpanded,
         })}
       >
-        <button
-          className={classes.AppButton}
-          type="button"
-          onClick={(e) => toggleTheme(e.clientX, e.clientY)}
-          title={t("common.toggle-theme")}
-          aria-label={t("common.toggle-theme")}
-        >
-          {theme === "light" ? <LightThemeIcon /> : <DarkThemeIcon />}
-        </button>
+        <ThemeButton />
 
-        <button
-          className={classes.AppButton}
-          type="button"
-          onClick={toggleLanguage}
-          title={t("common.change-language")}
-          aria-label={t("common.change-language")}
-        >
-          <LanguageIcon />
-          {i18n.language === "en" ? "en" : "ru"}
-        </button>
+        <LanguageButton />
 
         <AuthGuard forRoles={[]}>
-          <button
-            className={clsx(classes.AppButton, classes.LogoutButton)}
-            onClick={onLogout}
-            title={t("common.logout")}
-            aria-label={t("common.logout")}
-          >
-            <LogoutIcon />
-          </button>
+          <LogoutButton />
         </AuthGuard>
       </div>
     </aside>
+  );
+};
+
+const ThemeButton = () => {
+  const { osAPI } = useAPIContext();
+  const { theme, toggleTheme } = useThemeContext();
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    // run after changes in DOM
+    Promise.resolve().then(() => {
+      const bodyStyle = getComputedStyle(document.body);
+      const backgroundColor = bodyStyle.getPropertyValue("--color-background");
+      const textColor = bodyStyle.getPropertyValue("--color-text");
+      osAPI.setTitleBarSettings({
+        backgroundColor: `${backgroundColor}20`,
+        symbolColor: textColor,
+      });
+    });
+  }, [theme, osAPI]);
+
+  return (
+    <button
+      className={classes.AppButton}
+      type="button"
+      onClick={(e) => toggleTheme(e.clientX, e.clientY)}
+      title={t("common.toggle-theme")}
+      aria-label={t("common.toggle-theme")}
+    >
+      {theme === "light" ? <LightThemeIcon /> : <DarkThemeIcon />}
+    </button>
+  );
+};
+
+const LanguageButton = () => {
+  const { t, i18n } = useTranslation();
+  const [language, setLanguage] = usePersistedState<string>(
+    "language",
+    i18n.language,
+  );
+
+  useEffect(() => {
+    i18n.changeLanguage(language);
+    document.documentElement.lang = language;
+  }, [language]);
+
+  const toggleLanguage = () => {
+    const newLanguage = language === "en" ? "ru" : "en";
+    i18n.changeLanguage(newLanguage);
+    setLanguage(newLanguage);
+  };
+
+  return (
+    <button
+      className={classes.AppButton}
+      type="button"
+      onClick={toggleLanguage}
+      title={t("common.change-language")}
+      aria-label={t("common.change-language")}
+    >
+      <LanguageIcon />
+      {i18n.language === "en" ? "en" : "ru"}
+    </button>
+  );
+};
+
+const LogoutButton = () => {
+  const { logout } = useAuthContext();
+  const { notify } = useUIContext();
+  const { t } = useTranslation();
+
+  const onLogout = async () => {
+    try {
+      await logout();
+    } catch (error) {
+      notify.error(error);
+    }
+  };
+
+  return (
+    <button
+      className={clsx(classes.AppButton, classes.LogoutButton)}
+      onClick={onLogout}
+      title={t("common.logout")}
+      aria-label={t("common.logout")}
+    >
+      <LogoutIcon />
+    </button>
   );
 };
 
